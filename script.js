5cbbe8dd612c1054e7821a402f4715ae197b3148
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2020-12-27 20:12:23 -0500

    setup

---
+++ script.js
@@ -0,0 +1,18 @@
+function mlbStandings() {
+
+    const api_url = `https://api.sportsdata.io/v3/soccer/scores/json/Competitions?key=55f7015ef0324e349a50ff5aced16802`;
+    
+    fetch(api_url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            
+            console.log(data);
+    
+            
+    
+        })
+    }
+    
+    mlbStandings();
