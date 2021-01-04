e65d44ce9c8937c64f3f95a2935d3abc69122f4e
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-01-03 20:24:23 -0500

    update

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function mlbStandings() {
 
-    const api_url = `https://api.sportsdata.io/v3/soccer/scores/json/Competitions?key=55f7015ef0324e349a50ff5aced16802`;
+    const api_url = `https://api.sportsdata.io/v3/nhl/stats/json/BoxScores/%7B2020-Jan-13%7D?key=ae256145ae3b45d19da29122634bd4c4`;
     
     fetch(api_url)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            
+            console.log(data);
             
     
             
