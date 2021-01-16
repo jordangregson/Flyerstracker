7788fca0ce5a0f0f419e4d00ad3ebece4c34ca28
Jordan Gregson <49405277+jordangregson@users.noreply.github.com>
2021-01-16 15:47:51 -0500

    Added a standings page. Still working on the code

--- js/standings.js
+++ js/standings.js
@@ -1,13 +1,13 @@
 function mlbStandings() {
 
-    const api_url = ``;
+    const api_url = `https://api.sportsdata.io/v3/nhl/scores/json/Standings/2021?key=ae256145ae3b45d19da29122634bd4c4`;
     
     fetch(api_url)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            
+            console.log(data);
             
     
             
